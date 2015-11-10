87df14bb436fa2d465da75157dc58366e1210abe
Pablo Repetto <caussabon@gmail.com>
2015-11-10 18:32:15 -0300

    Creada la clase CalculadoraEnvido
    Creada para mantener la separacion entre las operaciones de la Mano
    y el calculo de valores concretos.

---
+++ src/fiuba/algo3/modelo/CalculadoraEnvido.java
@@ -0,0 +1,15 @@
+package src.fiuba.algo3.modelo;
+
+import java.util.Collection;
+import java.util.Arrays;
+import java.util.Set;
+import java.util.HashSet;
+
+public class CalculadoraEnvido
+{
+    public static int calcular(Carta ... cartas)
+    {
+        return 1;
+    }
+}
+
---
+++ test/fiuba/algo3/modelo/CalculadoraEnvidoTest.java
@@ -0,0 +1,23 @@
+package test.fiuba.algo3.modelo;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.Assert;
+
+import src.fiuba.algo3.modelo.CalculadoraEnvido;
+import src.fiuba.algo3.modelo.Carta;
+import src.fiuba.algo3.modelo.Palo;
+
+public class CalculadoraEnvidoTest
+{
+    @Test
+    public void testUnosDeDistintoPalo()
+    {
+        Assert.assertEquals(1,
+                CalculadoraEnvido.calcular(
+                        new Carta(Palo.Espada, 1),
+                        new Carta(Palo.Basto, 1),
+                        new Carta(Palo.Copa, 1)));
+    }
+}
+
