5b302a29a31848908005d079fd64a61b93cfa5f4
Pablo Repetto <caussabon@gmail.com>
2015-11-11 14:27:00 -0300

    Nueva prueba. testGanaJ1
    Probando la generalizacion de Mano a tres jugadores.

---
+++ test/fiuba/algo3/modelo/RondaDeTresTest.java
@@ -0,0 +1,56 @@
+package test.fiuba.algo3.modelo;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.Assert;
+
+import src.fiuba.algo3.modelo.Ronda;
+import src.fiuba.algo3.modelo.Jugador;
+import src.fiuba.algo3.modelo.Carta;
+import src.fiuba.algo3.modelo.Palo;
+import src.fiuba.algo3.modelo.CartaVersus;
+
+public class RondaDeTresTest
+{
+    private Ronda R;
+    private Jugador j1, j2, j3;
+
+    @Before
+    public void setUp()
+    {
+        R = new Ronda();
+        j1 = new Jugador();
+        j2 = new Jugador();
+        j3 = new Jugador();
+    }
+
+    @Test
+    public void testGanaJ1()
+    {
+        R.jugar(j1, new Carta(Palo.Espada, 1));
+        R.jugar(j2, new Carta(Palo.Oro, 10));
+        R.jugar(j3, new Carta(Palo.Oro, 6));
+        
+        Assert.assertEquals(j1, R.getGanador());
+    }
+/*
+    @Test
+    public void testGanaJ2()
+    {
+        R.jugar(j1, new Carta(Palo.Oro, 7));
+        R.jugar(j2, new Carta(Palo.Espada, 7));
+        
+        Assert.assertEquals(j2, R.getGanador());
+    }
+/*
+    @Test
+    public void testEmpardan()
+    {
+        R.jugar(j1, new Carta(Palo.Copa, 2));
+        R.jugar(j2, new Carta(Palo.Basto, 2));
+        
+        Assert.assertEquals(null, R.getGanador());
+    }
+*/
+}
+
