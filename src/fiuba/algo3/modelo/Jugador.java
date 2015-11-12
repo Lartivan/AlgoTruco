325d55036e2ed23cbd17d39afc3262790d1958e2
Pablo Repetto <caussabon@gmail.com>
2015-11-12 13:38:47 -0300

    Nueva prueba. testJugarCartaCasoFelizNoLanzaExcepcion

--- src/fiuba/algo3/modelo/Jugador.java
+++ src/fiuba/algo3/modelo/Jugador.java
@@ -2,9 +2,17 @@ package src.fiuba.algo3.modelo;
 
 public class Jugador
 {
+    private Mano mano;
+    
     public void jugar(Carta c)
     {
-        throw new RuntimeException("El Jugador no tiene cartas");
+        if (mano == null)
+            throw new RuntimeException("El Jugador no tiene cartas");
+    }
+
+    public void repartir(Mano unaMano)
+    {
+        mano = unaMano;
     }
 }
 
--- test/fiuba/algo3/modelo/JugadorTest.java
+++ test/fiuba/algo3/modelo/JugadorTest.java
@@ -5,6 +5,7 @@ import org.junit.Test;
 import org.junit.Assert;
 
 import src.fiuba.algo3.modelo.Jugador;
+import src.fiuba.algo3.modelo.Mano;
 import src.fiuba.algo3.modelo.Carta;
 import src.fiuba.algo3.modelo.Palo;
 
@@ -21,6 +22,15 @@ public class JugadorTest
     @Test(expected = RuntimeException.class)
     public void testJugadorSinCartasNoPuedeJugar()
     {
+        J.jugar(new Carta(Palo.Espada, 1));
+    }
+
+    @Test
+    public void testJugarCartaCasoFelizNoLanzaExcepcion()
+    {
+        J.repartir(new Mano(new Carta(Palo.Espada, 1),
+                new Carta(Palo.Espada, 2),
+                new Carta(Palo.Espada, 3)));
         J.jugar(new Carta(Palo.Espada, 1));
     }
 
