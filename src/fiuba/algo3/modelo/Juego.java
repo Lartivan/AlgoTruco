11b1b0c30ff19135e29d79cb0ee455ba42ff544a
Pablo Repetto <caussabon@gmail.com>
2015-11-14 17:34:54 -0300

    Nueva prueba. testJugadoresEmpiezanSinCartas

---
+++ src/fiuba/algo3/modelo/Juego.java
@@ -0,0 +1,20 @@
+package src.fiuba.algo3.modelo;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.ArrayList;
+
+public class Juego
+{
+    private List<Jugador> jugadores;
+
+    public Juego(Jugador ... jugadores)
+    {
+    }
+
+    public Collection<Carta> getCartas(Jugador unJugador)
+    {
+        return null;
+    }
+}
+
---
+++ test/fiuba/algo3/modelo/JuegoTest.java
@@ -0,0 +1,38 @@
+package test.fiuba.algo3.modelo;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.Assert;
+
+import src.fiuba.algo3.modelo.Juego;
+import src.fiuba.algo3.modelo.Jugador;
+import src.fiuba.algo3.modelo.Carta;
+import src.fiuba.algo3.modelo.Palo;
+
+import java.util.Collection;
+
+public class JuegoTest
+{
+    Jugador j1, j2;
+    Juego J;
+
+    @Before
+    public void setUp()
+    {
+        j1 = new Jugador();
+        j2 = new Jugador();
+
+        J = new Juego(j1, j2);
+    }
+
+    @Test
+    public void testJugadoresEmpiezanSinCartas()
+    {
+        Collection<Carta> cartasJ1 = J.getCartas(j1);
+        Collection<Carta> cartasJ2 = J.getCartas(j2);
+        
+        Assert.assertEquals(null, cartasJ1);
+        Assert.assertEquals(null, cartasJ2);
+    }
+}
+
