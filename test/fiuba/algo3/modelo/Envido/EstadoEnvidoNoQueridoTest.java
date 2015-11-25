462b4d641f8d39363fec437cbfc2278495eee4c3
Pablo Repetto <caussabon@gmail.com>
2015-11-25 05:17:14 -0300

    Nueva prueba. testNoPuedeCantarEnvido

---
+++ test/fiuba/algo3/modelo/Envido/EstadoEnvidoNoQueridoTest.java
@@ -0,0 +1,33 @@
+package test.fiuba.algo3.modelo;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.Assert;
+
+import src.fiuba.algo3.modelo.EstadoEnvido;
+import src.fiuba.algo3.modelo.EstadoEnvidoNoQuerido;
+import src.fiuba.algo3.modelo.Ciclo;
+import src.fiuba.algo3.modelo.Jugador;
+
+public class EstadoEnvidoNoQueridoTest
+{
+    Ciclo ciclo;
+    EstadoEnvido estado;
+    Jugador j1;
+    
+    @Before
+    public void setUp()
+    {
+        estado = new EstadoEnvidoNoQuerido();
+        ciclo = new Ciclo();
+        ciclo.setEstadoEnvido(estado);
+        j1 = new Jugador();
+    }
+    
+    @Test
+    public void testNoPuedeCantarEnvido()
+    {
+        Assert.assertFalse(ciclo.cantarEnvido(j1));
+    }
+}
+
